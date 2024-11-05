3fa89d7fca2d7fd22b3205e080ce3568344a63eb
Carlos Tolosa <carloswtolosa@gmail.com>
2024-11-05 20:07:43 -0300

    Implementacion de Arbol de Huffman

--- Huffman.cpp
+++ Huffman.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "Huffman.h"
+#include <queue>
+#include <vector>
+#include <bitset>
+
+nodoHuffman* crearNodo(char caracter, int frecuencia, nodoHuffman* izq, nodoHuffman* der){
+    nodoHuffman* nodo = new nodoHuffman();
+    nodo->caracter = caracter;
+    nodo->frecuencia = frecuencia;
+    nodo->izq = izq;
+    nodo->der = der;
+
+    return nodo;
+}
+
+
+nodoHuffman* arbolHuffman(const std::unordered_map<char, int>& frecuencia){
+    std::priority_queue<nodoHuffman*, std::vector<nodoHuffman*>, comparadorFrecuencia>colaMinima;
+    for (const auto& entrada : frecuencia) {
+        colaMinima.push(crearNodo(entrada.first, entrada.second, nullptr, nullptr));
+    }
+
+    while (colaMinima.size() > 1){
+        nodoHuffman* izq = colaMinima.top();
+        colaMinima.pop();
+        nodoHuffman* der = colaMinima.top();
+        colaMinima.pop();
+        int suma = izq->frecuencia + der->frecuencia;
+        colaMinima.push(crearNodo('\0', suma, izq, der));
+    }
+
+    return colaMinima.top();
+}
+
+
+void generarCodigo(nodoHuffman* ruta, const std::string& caracter, std::unordered_map<char, std::string>& codigoHuffman){
+    if(!ruta){
+        return;
+    }
+    if(!ruta->izq && !ruta->der){
+        codigoHuffman[ruta->caracter] = caracter;
+    }
+    generarCodigo(ruta->izq, caracter + "0", codigoHuffman);
+    generarCodigo(ruta->der, caracter + "1", codigoHuffman);
+}
+
--- Huffman.h
+++ Huffman.h
@@ -4,5 +4,27 @@
 
 #ifndef TPFINAL_HUFFMAN_H
 #define TPFINAL_HUFFMAN_H
+#include <iostream>
+#include <unordered_map>
+
+struct nodoHuffman{ //Define un nodo del arbol de Huffman que tiene un caracter, su frecuencia y punteros a los dos nodos hijos
+    char caracter;
+    int frecuencia;
+    nodoHuffman *izq;
+    nodoHuffman *der;
+};
+
+struct comparadorFrecuencia{
+    bool operator()(nodoHuffman* izq, nodoHuffman* der){ // Se compara el valor de la frecuencia entre ambos nodos hijos
+        return izq->frecuencia > der->frecuencia;
+    }
+};
+
+nodoHuffman* crearNodo(char caracter, int frecuencia, nodoHuffman* izq, nodoHuffman* der);
+nodoHuffman* arbolHuffman(const std::unordered_map<char, int>& frecuencia); // Crea el arbol usando una cola de prioridad segun la frecuencia de los caracteres
+void generarCodigo(nodoHuffman* ruta, std::string& caracter, std::unordered_map<char, std::string>& codigoHuffman);
+
+
+
 
 #endif //TPFINAL_HUFFMAN_H
